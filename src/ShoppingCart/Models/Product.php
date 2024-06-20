5f0be371961b56b1d231f4d1ae4a241bfa292046
kapcruz <karina.pereira@feedz.com.br>
2024-06-20 13:47:25 -0300

    feat: model do produto

--- src/ShoppingCart/Models/Product.php
+++ src/ShoppingCart/Models/Product.php
@@ -1,5 +1,21 @@
 <?php
 namespace App\ShoppingCart\Models;
 
-class Product extends \Illuminate\Database\Eloquent\Model {}
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends \Illuminate\Database\Eloquent\Model {
+
+    use SoftDeletes;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        "name",
+        "price",
+        "quantity",
+        "slug",
+        "code",
+        "image"
+    ];
+}
 
