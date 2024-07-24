a6f15f45535d047481b4dee451b878d36f070aac
kapcruz <karina.pereira@feedz.com.br>
2024-07-24 17:33:52 -0300

    feat: implementando teste de crianção de usuário

--- tests/ShoppingCart/Application/Repositories/UserRepositoryTest.php
+++ tests/ShoppingCart/Application/Repositories/UserRepositoryTest.php
@@ -4,13 +4,26 @@ namespace ShoppingCart\Application\Repositories;
 
 use App\Core\ShoppingCart\Domain\Entities\Cpf;
 use App\Core\ShoppingCart\Domain\Entities\User;
+use App\ShoppingCart\Models\User as UserModel;
 use App\Core\ShoppingCart\Domain\ValueObject\Role;
 use App\Core\ShoppingCart\Domain\ValueObject\Status;
 use App\Core\ShoppingCart\Application\Repositories\UserRepository;
 use AppTest\ShoppingCartTestCase;
 
 class UserRepositoryTest extends ShoppingCartTestCase
-{
+{  
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        UserModel::where('email', 'fernando@email.com')->forceDelete();
+    }
+    
     public function testCreateNewUser()
     {
         $userRepository = new UserRepository();
@@ -22,5 +35,9 @@ class UserRepositoryTest extends ShoppingCartTestCase
             new Status(Status::STATUS_ENABLE)
         );
         $userRepository->create($user);
+
+        $user = UserModel::where('email', 'fernando@email.com')->get()->first();
+
+        $this->assertNotNull($user);
     }
 }
