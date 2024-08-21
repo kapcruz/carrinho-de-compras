a76d58d63f9dc125565e60947b0d42298d2143c1
Lana Schuster <lana.schuster@feedz.com.br>
2024-08-21 17:26:15 -0300

    feat: add campos na tabela orders

--- infrastructure/database/migrations/20240814205030_order_table.php
+++ infrastructure/database/migrations/20240814205030_order_table.php
@@ -10,15 +10,23 @@ final class OrderTable extends AbstractMigration
     public function up(): void
     {
         $this->table('orders', ['id' => 'id'])
-        ->addColumn('delivery_address', 'string', ['null' => false])
-        ->addColumn('created_at', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
-        ->addColumn('updated_at', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP'])
-        ->addColumn('deleted_at', 'datetime', ['null' => true])
-        ->create();
+            ->addColumn('id_user', 'integer', ['null' => false, 'signed' => false])
+            ->addColumn('code', 'string', ['null' => false])
+            ->addColumn('total_price', 'double', ['null' => false])
+            ->addColumn('delivery_address', 'string', ['null' => false])
+            ->addColumn('status', 'integer', ['null' => false])
+            ->addColumn('created_at', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('deleted_at', 'datetime', ['null' => true])
+            ->addForeignKey('id_user', 'users', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
+            ->create();
     }
 
     public function down(): void
     {
-
+        $this->table('orders')
+            ->dropForeignKey('id_user')
+            ->drop()
+            ->save();
     }
 }
